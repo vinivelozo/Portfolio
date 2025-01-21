4121682bbf0f637e8d987037d8de02065865474f
Vinicius Velozo de Sousa <vinivelozo02@outlook.com>
2025-01-21 11:16:44 -0500

    Initial Commit

---
+++ portfolioBe/src/main/java/com/portfolioBe/portfolioBe/PortfolioBeApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioBe.portfolioBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBeApplication.class, args);
+	}
+
+}
---
+++ portfolioBe/src/test/java/com/portfolioBe/portfolioBe/PortfolioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioBe.portfolioBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
